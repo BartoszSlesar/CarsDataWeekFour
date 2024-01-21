85bfdebd33a7935a77a32489b9968855e35a2068
bard <bartoszslesar@gmail.com>
2024-01-21 18:54:50 +0100

    split project to 2 modules, added FO templates

---
+++ carsdata-fo/src/App.js
@@ -0,0 +1,13 @@
+import NavigationBar from "./components/NavigationBar";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <div className="App">
+            <NavigationBar/>
+            <Header/>
+        </div>
+    );
+}
+
+export default App;
---
+++ carsdata-fo/src/components/Header.js
@@ -0,0 +1,12 @@
+import "../css/style.css"
+export default function Header(){
+    return (
+        <header className="bg-dark py-5">
+            <div className="container px-4 px-lg-5 my-5">
+                <div className="text-center text-white">
+                    <h1 className="display-4 fw-bolder">CARS DATA</h1>
+                </div>
+            </div>
+        </header>
+    );
+}
